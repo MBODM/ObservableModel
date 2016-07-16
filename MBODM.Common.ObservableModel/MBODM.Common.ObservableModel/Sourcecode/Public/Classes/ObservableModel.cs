b58f7a2c309820ab06dbfb4d0916309e2d0b6042
MBODM <webmaster@mbodm.com>
2016-07-16 07:44:54 +0200

    Initial commit

---
+++ MBODM.Common.ObservableModel/MBODM.Common.ObservableModel/Sourcecode/Public/Classes/ObservableModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MBODM.Common
+{
+    public abstract class ObservableModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Set<T>(ref T oldValue, T newValue, [CallerMemberName] string propertyName = "")
+        {
+            if (object.Equals(oldValue, newValue))
+            {
+                return;
+            }
+
+            oldValue = newValue;
+
+            OnPropertyChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentException("Argument is null or empty.", nameof(propertyName));
+            }
+
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
